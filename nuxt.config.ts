47dd92d54755ba5255276809585337bebec76692
仲山慎之助 <nakayamashinnosuke@nakayamashinnosukenoMacBook-Air.local>
2024-08-11 23:27:33 +0900

    【ブログデザイン作成】vutifyインストール[in progress]

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,5 +19,22 @@ export default defineNuxtConfig({
   typescript: {
     strict: true,
     typeCheck: true
+  },
+  css: [
+    "vuetify/lib/styles/main.sass"
+  ],
+  build: {
+      transpile: ["vuetify"]
+  },
+  vite: {
+    define: {
+        "process.env.DEBUG": false
+    },
+    // for HMR
+    server: {
+        watch: {
+            usePolling: true
+        }
+    },
   }
 })
---
+++ pages/about.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-container>
+        <h2>{{ title }}</h2>
+        <v-btn @click="pushButton">
+            PUSH!
+        </v-btn>
+    </v-container>
+</template>
+<script>
+export default {
+    data: () => ({
+        title: "This is the test page."
+    }),
+    methods: {
+        pushButton(){
+            this.title = "PUSHED!";
+        }
+    }
+}
+</script>
