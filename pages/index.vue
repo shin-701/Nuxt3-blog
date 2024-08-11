345d3e0e344d6207c060e7ecc77853b4d45a83cf
仲山慎之助 <nakayamashinnosuke@nakayamashinnosukenoMacBook-Air.local>
2024-08-11 22:09:40 +0900

    【ブログデザイン作成】一覧画面に表示するコンポーネント追加

--- pages/index.vue
+++ pages/index.vue
@@ -12,6 +12,15 @@
     次に、SDKが提供している getContents メソッドを利用して、投稿一覧を取得します。getContentsのパラメータに Article の型を渡すことで、返却される items の型として Article[] が指定されます。
     また、selectパラメータを利用して、取得するフィールドを _id・title・slug・body のみに制限します。
     最後に articles として data.value?.items を定義することで、getContentsで返却されたオブジェクトの items 要素を articles として参照できるようにします。
+ ------------
+ ◆フィールドID
+    タイトル： title
+    スラッグ： slug
+    メタ情報： meta
+    本文： body
+    カバー画像： coverImage
+    著者： author
+    タグ： tags
      =====================================-->
 <script lang="ts" setup>
 import type { Article } from '~/types/article'
@@ -21,9 +30,9 @@ const { data } = await useAsyncData('articles', async () => {
   return await $newtClient.getContents<Article>({
     appUid: 'blog',
     modelUid: 'article',
-    query: {
-      select: ['_id', 'title', 'slug', 'body']
-    }
+    // query: {
+    //   select: ['_id', 'title', 'slug', 'body', 'coverImage']
+    // }
   })
 })
 const articles = data.value?.items
@@ -37,13 +46,174 @@ useHead({
 </script>
 
 <template>
-    <div>
-      <ul>
-        <li v-for="article in articles" :key="article._id">
-          <NuxtLink :to="`/articles/${article.slug}`">
-            {{ article.title }}
-          </NuxtLink>
-        </li>
-      </ul>
-    </div>
+  <div v-for="article in articles" :key="article._id">
+    <NuxtLink :to="`/articles/${article.slug}`" class="Article">
+      <div class="Article_Eyecatch">
+        <img alt="" loading="lazy" width="1000" height="667" decoding="async" data-nimg="1" v-bind:src="article.coverImage.src" style="color: transparent;">
+      </div>
+      <div class="Article_Data">
+        <h3 class="Article_Title">{{ article.title }}</h3>
+        <ul class="Article_Tags">
+          <li v-for="tag in article.tags" :key="tag._id">
+              #{{ tag.name }}
+          </li>
+        </ul>
+        <div class="Article_Author">
+          <div class="Article_AuthorEmpty">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 24 24" fill="#CCCCCC">
+              <path d="M0 0h24v24H0V0z" fill="none"></path>
+              <path d="M12 6c1.1 0 2 .9 2 2s-.9 2-2 2-2-.9-2-2 .9-2 2-2m0 10c2.7 0 5.8 1.29 6 2H6c.23-.72 3.31-2 6-2m0-12C9.79 4 8 5.79 8 8s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 10c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path>
+            </svg>
+          </div>
+          <div class="Article_AuthorData">
+            <span>Donna Thomason</span>
+            <time datetime="12/9/2021">12/9/2021</time>
+          </div>
+        </div>
+      </div>
+    </NuxtLink>
+  </div>
 </template>
+
+<style scoped>
+.Article {
+  display: block;
+  align-items: center;
+  color: #333;
+  text-decoration: none;
+  overflow: hidden;
+  padding: 0;
+  margin: 0 0 40px 0;
+}
+.Article:last-child {
+  margin: 0 0 28px 0;
+}
+.Article_Eyecatch {
+  width: 100%;
+  height: 0;
+  padding: 52.5% 0 0 0;
+  border-radius: 4px;
+  overflow: hidden;
+  flex-shrink: 0;
+  margin: 0 0 12px 0;
+  position: relative;
+}
+.Article_Eyecatch > img {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.Article_EyecatchEmpty {
+  background: #f8f8f8;
+  width: 280px;
+  height: 147px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.Article_Data {
+  flex: 1;
+}
+.Article_Title {
+  font-size: 1.8rem;
+  line-height: 1.5;
+  margin: 0 0 10px;
+  padding: 0;
+  display: -webkit-box;
+  overflow: hidden;
+  -line-clamp: 2;
+  -webkit-box-orient: vertical;
+}
+.Article_Tags {
+  margin: 0 0 8px;
+  padding: 0;
+  display: flex;
+  flex-wrap: wrap;
+}
+.Article_Tags li {
+  margin: 0 4px 4px 0;
+  padding: 0 4px;
+  list-style: none;
+  font-size: 1.2rem;
+  color: #666;
+  border: 1px solid #e5e5e5;
+  border-radius: 4px;
+}
+.Article_Author {
+  display: flex;
+  align-items: center;
+}
+.Article_Author > img {
+  width: 32px;
+  height: 32px;
+  border-radius: 16px;
+  margin: 0 12px 0 0;
+  flex-shrink: 0;
+}
+.Article_Author img {
+  width: 32px;
+  height: 32px;
+  object-fit: cover;
+  font-family: 'object-fit: cover'; /* IE11 */
+}
+.Article_AuthorEmpty {
+  width: 32px;
+  height: 32px;
+  background: #f8f8f8;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 16px;
+  margin: 0 12px 0 0;
+  flex-shrink: 0;
+}
+.Article_AuthorData {
+  flex: 1;
+}
+.Article_AuthorData span {
+  font-size: 1.2rem;
+  font-weight: 700;
+  display: block;
+  line-height: 1.5;
+}
+.Article_AuthorData time {
+  font-size: 1.2rem;
+  display: block;
+  color: #666;
+  line-height: 1.5;
+}
+@media (min-width: 600px) {
+  .Article {
+    display: flex;
+    border-bottom: 1px solid #e5e5e5;
+    padding: 0 0 36px;
+    margin: 0 0 36px;
+  }
+  .Article:hover .Article_Title {
+    text-decoration: underline;
+  }
+  .Article:last-child {
+    border-bottom: none;
+    padding: 0 0 20px 0;
+  }
+  .Article_Eyecatch {
+    width: 280px;
+    height: 147px;
+    margin: 0 28px 0 0;
+    padding: 0;
+  }
+  .Article_Eyecatch img {
+    width: 280px;
+    height: 147px;
+  }
+  .Article_Title {
+    margin: 0 0 12px;
+  }
+  .Article_Tags {
+    margin: 0 0 16px;
+  }
+}
+</style>
--- types/article.ts
+++ types/article.ts
@@ -1,6 +1,22 @@
-export interface Article {
-    _id: string
-    title: string
-    slug: string
-    body: string
+import type { Content, Media } from 'newt-client-js'
+import type { Author } from '@/types/author'
+import type { Tag } from '@/types/tag'
+
+export interface Article extends Content {
+  title: string
+  slug: string
+  meta?: {
+    title?: string
+    description?: string
+    ogImage?: Media
   }
+  body: string
+  coverImage: Media
+  author: Author
+  tags?: Tag[]
+}
+
+export interface Archive {
+  year: number
+  count: number
+}
